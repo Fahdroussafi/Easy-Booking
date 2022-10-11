7943f1af02b767fa39b1d85c499da07624352631
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-11 15:04:24 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+function App() {
+  return (
+    <div className="App m-5 p-5">
+      <h1 className="text-2xl text-red-500">Easy-Booking</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
+
+export default App;
