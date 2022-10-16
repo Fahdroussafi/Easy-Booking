5e563ab0d0772066357dd7c960bc9cf37f32c4d8
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 22:56:53 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const dbConfig = require("./config/dbConfig");
+
+app.use(cors());
 app.use(express.json());
 
 const usersRoute = require("./routes/usersRoute");
