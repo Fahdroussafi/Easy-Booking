05a6086354750b1848bab7adfb5cbbbc9025e4fe
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 22:56:41 +0100

    Update AdminBuses.jsx

--- client/src/pages/Admin/AdminBuses.jsx
+++ client/src/pages/Admin/AdminBuses.jsx
@@ -1,24 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 import BusForm from "../../components/BusForm";
 import PageTitle from "../../components/PageTitle";
+import { HideLoading, ShowLoading } from "../../redux/alertsSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { axiosInstance } from "../../helpers/axiosInstance";
+import { message, Table } from "antd";
 
 function AdminBuses() {
-  const [showBusForm, setShowBusForm] = React.useState(false);
+  const dispatch = useDispatch();
+  const [showBusForm, setShowBusForm] = useState(false);
+  const [buses, setBuses] = useState([]);
+  const [selectedBus, setSelectedBus] = useState(null);
+
+  const getBuses = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post("/api/buses/get-all-buses", {});
+      dispatch(HideLoading());
+      if (response.data.success) {
+        setBuses(response.data.data);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  const deleteBus = async (_id) => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post("/api/buses/delete-bus", {
+        _id: _id,
+      });
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+        getBuses();
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Bus Number",
+      dataIndex: "busNumber",
+    },
+    {
+      title: "From",
+      dataIndex: "from",
+    },
+    {
+      title: "To",
+      dataIndex: "to",
+    },
+    {
+      title: "Journey Date",
+      dataIndex: "journeyDate",
+    },
+
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (actions, record) => (
+        <div className="flex gap-3">
+          <i
+            class="ri-delete-bin-line cursor-pointer"
+            onClick={() => {
+              deleteBus(record._id);
+            }}
+          ></i>
+
+          <i
+            class="ri-pencil-line cursor-pointer"
+            onClick={() => {
+              setSelectedBus(record);
+              setShowBusForm(true);
+            }}
+          ></i>
+        </div>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    getBuses();
+  }, []);
 
   return (
-    <div className="flex justify-between">
-      <PageTitle title="Buses" />
-      <button
-        className="btn btn-primary bg-blue-600 hover:bg-blue-800"
-        onClick={() => setShowBusForm(true)}
-      >
-        Add Bus
-      </button>
+    <div>
+      <div className="flex justify-between">
+        <PageTitle title="Buses" />
+        <button
+          className="btn btn-primary bg-blue-600 hover:bg-blue-800"
+          onClick={() => setShowBusForm(true)}
+        >
+          Add Bus
+        </button>
+      </div>
+
+      <Table columns={columns} dataSource={buses} />
       {showBusForm && (
         <BusForm
           showBusForm={showBusForm}
           setShowBusForm={setShowBusForm}
-          type="add"
+          type={selectedBus ? "edit" : "add"}
+          selectedBus={selectedBus}
+          setSelectedBus={setSelectedBus}
+          getData={getBuses}
         />
       )}
     </div>
