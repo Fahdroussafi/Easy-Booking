574ad523831bccc3822df6c90e050a95b4f028a6
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 22:56:58 +0100

    Update busesRoute.js

--- server/routes/busesRoute.js
+++ server/routes/busesRoute.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const Bus = require("../models/busModel");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // Add a new bus
-
 router.post("/add-bus", async (req, res) => {
   try {
     const existingBus = await Bus.findOne({ busNumber: req.body.busNumber });
@@ -16,6 +16,46 @@ router.post("/add-bus", async (req, res) => {
     await newBus.save();
     res.status(200).send({
       message: "Bus created successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
+
+// get all buses
+router.post("/get-all-buses", authMiddleware, async (req, res) => {
+  try {
+    const buses = await Bus.find();
+    return res.status(200).send({
+      success: true,
+      message: "Buses fetched successfully",
+      data: buses,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
+
+// update a bus
+router.post("/update-bus", authMiddleware, async (req, res) => {
+  try {
+    await Bus.findByIdAndUpdate(req.body._id, req.body);
+    res.status(200).send({
+      message: "Bus updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
+
+// delete a bus
+router.post("/delete-bus", authMiddleware, async (req, res) => {
+  try {
+    await Bus.findByIdAndDelete(req.body._id);
+    res.status(200).send({
+      message: "Bus deleted successfully",
       success: true,
     });
   } catch (error) {
