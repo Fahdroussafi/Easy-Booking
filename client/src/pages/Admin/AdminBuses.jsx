7ba41fbfe4accd9ebd0550e82bb9617bf0624b9e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 19:34:34 +0100

    Update AdminBuses.jsx

--- client/src/pages/Admin/AdminBuses.jsx
+++ client/src/pages/Admin/AdminBuses.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import BusForm from "../../components/BusForm";
+import PageTitle from "../../components/PageTitle";
 
 function AdminBuses() {
+  const [showBusForm, setShowBusForm] = React.useState(false);
+
   return (
-    <div>AdminBuses</div>
-  )
+    <div className="flex justify-between">
+      <PageTitle title="Buses" />
+      <button
+        className="btn btn-primary bg-blue-600 hover:bg-blue-800"
+        onClick={() => setShowBusForm(true)}
+      >
+        Add Bus
+      </button>
+      {showBusForm && (
+        <BusForm
+          showBusForm={showBusForm}
+          setShowBusForm={setShowBusForm}
+          type="add"
+        />
+      )}
+    </div>
+  );
 }
 
-export default AdminBuses
+export default AdminBuses;
