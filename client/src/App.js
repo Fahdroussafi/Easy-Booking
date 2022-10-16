66560c4efc5d21688e450c3a0b2653edf54ecbb4
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 03:55:08 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import Home from "./pages/Home";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Loader from "./components/Loader";
 import PublicRoute from "./components/PublicRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <div className="App">
+      {loading && <Loader />}
       <BrowserRouter>
         <Routes>
           <Route
