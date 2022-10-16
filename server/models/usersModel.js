d0a1b2f502f3cd3ad60f25ee96d518c4adf3c534
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 01:02:26 +0100

    Create usersModel.js

---
+++ server/models/usersModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("users", userSchema);
