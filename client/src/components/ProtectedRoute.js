b1a18b93300da8bbbc4ce9889f88a0b3454d6210
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 22:56:27 +0100

    Update ProtectedRoute.js

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -9,7 +9,7 @@ import DefaultLayout from "./DefaultLayout";
 
 function ProtectedRoute({ children }) {
   const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.alerts);
+  const { user } = useSelector((state) => state.users);
   const navigate = useNavigate();
   const validateToken = async () => {
     try {
@@ -48,7 +48,7 @@ function ProtectedRoute({ children }) {
     }
   }, []);
 
-  return <div>{!loading && <DefaultLayout>{children}</DefaultLayout>}</div>;
+  return <div>{user && <DefaultLayout>{children}</DefaultLayout>}</div>;
 }
 
 export default ProtectedRoute;
