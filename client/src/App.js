e7ff0e7e61a85e83b2d0e0aef697bac51ea54be5
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-16 01:06:38 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
 import "./index.css";
+import "antd/dist/antd.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PublicRoute from "./components/PublicRoute";
+
 function App() {
   return (
-    <div className="App m-5 p-5">
-      <h1 className="text-2xl text-red-500">Easy-Booking</h1>
-      <button className="btn btn-primary">Click me</button>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <Index />
+              </PublicRoute>
+            }
+          />
+
+          {/* <Route path="/home" element={<Home />} /> // PrivateRoute */}
+
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
