7e4fbcefd498d6d90a0d80c03947b6ffa885e99a
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-19 20:00:37 +0100

    Create busController.js

---
+++ server/Controllers/busController.js
@@ -0,0 +1,78 @@
+const router = require("express").Router();
+const Bus = require("../models/busModel");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// Add a new bus
+const AddBus = async (req, res) => {
+  try {
+    const existingBus = await Bus.findOne({ busNumber: req.body.busNumber });
+    existingBus
+      ? res.send({ message: "Bus already exists", success: false, data: null })
+      : await new Bus(req.body).save();
+
+    const newBus = new Bus(req.body);
+    await newBus.save();
+    res.status(200).send({
+      message: "Bus created successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+// get all buses
+const GetAllBuses = async (req, res) => {
+  try {
+    const buses = await Bus.find();
+    return res.status(200).send({
+      success: true,
+      message: "Buses fetched successfully",
+      data: buses,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+// update a bus
+const UpdateBus = async (req, res) => {
+  try {
+    await Bus.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).send({
+      message: "Bus updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+// delete a bus
+const DeleteBus = async (req, res) => {
+  try {
+    await Bus.findByIdAndDelete(req.params.id);
+    res.status(200).send({
+      message: "Bus deleted successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+// get bus by id
+const GetBusById = async (req, res) => {
+  try {
+    const bus = await Bus.findById(req.params.id);
+    res.status(200).send({
+      message: "Bus fetched successfully",
+      success: true,
+      data: bus,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
+
+module.exports = { AddBus, GetAllBuses, UpdateBus, DeleteBus, GetBusById };
