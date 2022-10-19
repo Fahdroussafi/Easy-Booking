0e8cc7078a1998d5df90a27824a16f7febf4a17e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-19 20:00:34 +0100

    Create usersController.js

---
+++ server/Controllers/usersController.js
@@ -0,0 +1,42 @@
+const User = require("../models/usersModel");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// get user by id
+
+const GetUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    res.send({
+      message: "User fetched successfully",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+      data: null,
+    });
+  }
+};
+
+
+// get all users
+const getAllClients = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send({
+      message: "Users fetched successfully",
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+      data: null,
+    });
+  }
+};
+
+module.exports = { GetUserById, getAllClients };
