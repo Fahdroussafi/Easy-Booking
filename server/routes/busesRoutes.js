355189229d41419363bd467f4d904a06473445a2
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-19 20:00:20 +0100

    Create busesRoutes.js

---
+++ server/routes/busesRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express();
+const {
+  AddBus,
+  GetAllBuses,
+  UpdateBus,
+  DeleteBus,
+  GetBusById
+} = require("../Controllers/busController");
+
+router.post("/add-bus", AddBus);
+router.get("/get-all-buses", GetAllBuses);
+router.put("/:id", UpdateBus);
+router.delete("/:id", DeleteBus);
+router.get("/:id", GetBusById);
+
+
+module.exports = router;
