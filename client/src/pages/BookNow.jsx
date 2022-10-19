2742bf7f941809f56f828ea3cc24f6f67f20ca27
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-19 20:00:51 +0100

    Update BookNow.jsx

--- client/src/pages/BookNow.jsx
+++ client/src/pages/BookNow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { axiosInstance } from "../helpers/axiosInstance";
 import { HideLoading, ShowLoading } from "../redux/alertsSlice";
 import { Row, Col, message } from "antd";
@@ -10,13 +10,11 @@ function BookNow() {
   const [selectedSeats, setSelectedSeats] = useState([]);
   const params = useParams();
   const dispatch = useDispatch();
-  const [bus, setBus] = useState({});
+  const [bus, setBus] = useState(null);
   const getBus = async () => {
     try {
       dispatch(ShowLoading());
-      const response = await axiosInstance.post("/api/buses/get-bus-by-id", {
-        _id: params.id,
-      });
+      const response = await axiosInstance.get(`/api/buses/${params.id}`);
       dispatch(HideLoading());
       if (response.data.success) {
         setBus(response.data.data);
@@ -28,6 +26,26 @@ function BookNow() {
       message.error(error.message);
     }
   };
+
+  const bookNow = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post("/api/bookings/book-seat", {
+        bus: bus._id,
+        seats: selectedSeats,
+      });
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
   useEffect(() => {
     getBus();
   }, []);
@@ -59,14 +77,31 @@ function BookNow() {
             <hr />
 
             <div className="flex w-48 flex-col ">
-              <h1 className="text-2xl">
+              <h1 className="text-lg mt-2">
+                Capacity : <b>{bus.capacity}</b>
+              </h1>
+              <h1 className="text-lg mt-2">
+                Seats Left :<b>{bus.capacity - bus.seatsBooked.length}</b>
+              </h1>
+            </div>
+            <hr />
+
+            <div className="flex flex-col gap-2 w-48 ">
+              <h1 className="text-xl">
                 <b>Selected Seats</b> : {selectedSeats.join(", ")}
               </h1>
-              <h1 className="text-2xl mt-2">
-                Price : <b>DH {bus.price * selectedSeats.length}</b>
+              <h1 className="text-xl mt-2">
+                <b> Price :</b> DH {bus.price * selectedSeats.length}
               </h1>
 
-              <button className="btn btn-primary bg-blue-600 hover:bg-blue-800">
+              <button
+                className={`btn btn-primary bg-blue-600 hover:bg-blue-800 ${
+                  selectedSeats.length === 0 &&
+                  "btn btn-primary cursor-not-allowed "
+                }`}
+                disabled={selectedSeats.length === 0}
+                onClick={bookNow}
+              >
                 Book Now
               </button>
             </div>
