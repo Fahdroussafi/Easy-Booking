46a050518c2c4849e39023293ad26c98ebcf33b1
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-25 00:52:27 +0100

    Update Bookings.jsx

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { axiosInstance } from "../helpers/axiosInstance";
-import { message } from "antd";
+import { message, Table } from "antd";
 import { HideLoading, ShowLoading } from "../redux/alertsSlice";
+import PageTitle from "../components/PageTitle";
 
 function Bookings() {
   const [bookings, setBookings] = useState([]);
@@ -12,12 +13,20 @@ function Bookings() {
     try {
       dispatch(ShowLoading());
       const response = await axiosInstance.get(
-        `/api/bookings/${localStorage.getItem("userId")}`,
+        `/api/bookings/${localStorage.getItem("user_id")}`,
         {}
       );
       dispatch(HideLoading());
       if (response.data.success) {
-        setBookings(response.data.data);
+        const mappedData = response.data.data.map((booking) => {
+          return {
+            ...booking,
+            ...booking.bus,
+            ...booking.user,
+            key: booking._id,
+          };
+        });
+        setBookings(mappedData);
       } else {
         message.error(response.data.message);
       }
@@ -27,11 +36,50 @@ function Bookings() {
     }
   };
 
+  const columns = [
+    {
+      title: "Bus Name",
+      dataIndex: "name",
+      key: "bus",
+    },
+    {
+      title: "Bus Number",
+      dataIndex: "busNumber",
+      key: "bus",
+    },
+    {
+      title: "Journey Date",
+      dataIndex: "journeyDate",
+    },
+    {
+      title: "Journey Time",
+      dataIndex: "departure",
+    },
+    {
+      title: "Seats",
+      dataIndex: "seats",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (text, record) => (
+        <div>
+          <h1 className="underline text-base">Print Ticket</h1>
+        </div>
+      ),
+    },
+  ];
+
   useEffect(() => {
     getBookings();
   }, []);
 
-  return <div>Bookings</div>;
+  return (
+    <div>
+      <PageTitle tt="Bookings" />
+      <Table columns={columns} dataSource={bookings} />
+    </div>
+  );
 }
 
 export default Bookings;
