c6eed830a9ae836c02c37f019362030eaab71973
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-24 20:09:27 +0100

    Update busesRoutes.js

--- server/routes/busesRoutes.js
+++ server/routes/busesRoutes.js
@@ -9,10 +9,10 @@ const {
 } = require("../Controllers/busController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post("/add-bus", authMiddleware, AddBus);
-router.get("/get-all-buses", authMiddleware, GetAllBuses);
-router.put("/:id", authMiddleware, UpdateBus);
-router.delete("/:id", authMiddleware, DeleteBus);
-router.get("/:id", authMiddleware, GetBusById);
+router.post("/add-bus", AddBus);
+router.get("/get-all-buses", GetAllBuses);
+router.put("/:id", UpdateBus);
+router.delete("/:id", DeleteBus);
+router.get("/:id", GetBusById);
 
 module.exports = router;
