8dce2dab2057dacf84f6efc4352df3180350d34b
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-24 20:08:45 +0100

    Update Bookings.jsx

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { axiosInstance } from "../helpers/axiosInstance";
+import { message } from "antd";
+import { HideLoading, ShowLoading } from "../redux/alertsSlice";
 
 function Bookings() {
-  return (
-    <div>Bookings</div>
-  )
+  const [bookings, setBookings] = useState([]);
+  const dispatch = useDispatch();
+
+  const getBookings = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.get(
+        `/api/bookings/${localStorage.getItem("userId")}`,
+        {}
+      );
+      dispatch(HideLoading());
+      if (response.data.success) {
+        setBookings(response.data.data);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getBookings();
+  }, []);
+
+  return <div>Bookings</div>;
 }
 
-export default Bookings
+export default Bookings;
