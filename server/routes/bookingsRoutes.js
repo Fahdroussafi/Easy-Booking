ba4805b3a9c7b8ac539d750312af2b0020353877
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-27 15:13:13 +0100

    Update bookingsRoutes.js

--- server/routes/bookingsRoutes.js
+++ server/routes/bookingsRoutes.js
@@ -10,10 +10,10 @@ const {
   PayWithStripe,
 } = require("../Controllers/bookingController");
 
-router.post("/book-seat", authMiddleware, BookSeat);
-router.get("/get-all-bookings", GetAllBookings);
+router.post("/book-seat/:userId", BookSeat);
+router.get("/get-all-bookings", authMiddleware, GetAllBookings);
 router.get("/:user_Id", GetAllBookingsByUser);
-router.delete("/:id", CancelBooking);
+router.delete("/:booking_id/:user_id/:bus_id", CancelBooking);
 router.post("/make-payment", PayWithStripe);
 
 module.exports = router;
