e1d56ff610e637507805dd7b72bd0648e029dbb8
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-04 15:37:37 +0100

    Update Index.jsx

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,11 +1,71 @@
 import "../assets/style/index.css";
 import image from "../assets/img/car.jpeg";
 import logo from "../assets/img/logo.png";
-import React from "react";
 import { Helmet } from "react-helmet";
-
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { axiosInstance } from "../helpers/axiosInstance";
+import { HideLoading, ShowLoading } from "../redux/alertsSlice";
+import Bus from "../components/Bus";
+import { Row, Col, message } from "antd";
 
 function Index() {
+  const dispatch = useDispatch();
+  const [buses, setBuses] = useState([]);
+  const [cities, setCities] = useState([]);
+  const { user } = useSelector((state) => state.users);
+  const [filters, setFilters] = useState({});
+
+  const getBusesByFilter = async () => {
+    dispatch(ShowLoading());
+    const from = filters.from;
+    const to = filters.to;
+    const journeyDate = filters.journeyDate;
+    try {
+      const { data } = await axiosInstance.post(
+        `/api/buses/get?from=${from}&to=${to}&journeyDate=${journeyDate}`
+      );
+
+      setBuses(data.data);
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.response.data.message);
+    }
+  };
+
+  const getBuses = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post("/api/buses/get-all-buses");
+      dispatch(HideLoading());
+      if (response.data.success) {
+        setBuses(response.data.data);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    axiosInstance.get("/api/cities/get-all-cities").then((response) => {
+      setCities(response.data.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (filters.from && filters.to && filters.journeyDate) getBuses();
+  }, []);
+
+  useEffect(() => {
+    if (filters.from && filters.to && filters.journeyDate) {
+      getBusesByFilter();
+    }
+  }, []);
+
   return (
     <>
       <Helmet>
@@ -40,15 +100,108 @@ function Index() {
                     className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white text-blue-600 transition duration-150 ease-in-out border border-transparent rounded-lg sm:w-auto sm:rounded-full bg-blue-50 xl:px-10 hover:bg-white focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 xl:text-xl"
                     data-unsp-sanitized="clean"
                   >
-                    Book Right Now
+                    Check Your Bookings
                   </a>
-                  {/* <a
-                    href="/register"
-                    className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white text-blue-600 transition duration-150 ease-in-out border border-transparent rounded-lg sm:w-auto sm:rounded-full bg-blue-50 xl:px-10 hover:bg-white focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 xl:text-xl"
-                    data-unsp-sanitized="clean"
-                  >
-                    Register
-                  </a> */}
+                </div>
+                <div>
+                  <div className="full my-5 mx-2 p-2 px-2 py-3 flex justify-center">
+                    <Row gutter={10} align="center">
+                      <Col lg={12} sm={24}>
+                        <select
+                          className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          onChange={(e) => {
+                            setFilters({ ...filters, from: e.target.value });
+                          }}
+                        >
+                          <option value="">From</option>
+                          {cities.map((data, index) => {
+                            return (
+                              <option key={index} value={data.ville}>
+                                {data.ville}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </Col>
+                      <Col lg={12} sm={24}>
+                        <select
+                          className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          onChange={(e) => {
+                            setFilters({ ...filters, to: e.target.value });
+                          }}
+                        >
+                          <option value="">To</option>
+                          {cities.map((data, index) => {
+                            return (
+                              <option key={index} value={data.ville}>
+                                {data.ville}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </Col>
+                      <Col lg={24} sm={24}>
+                        <input
+                          className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          min={new Date().toISOString().split("T")[0]}
+                          type="date"
+                          placeholder="Date"
+                          onChange={(e) => {
+                            setFilters({
+                              ...filters,
+                              journeyDate: e.target.value,
+                            });
+                          }}
+                        />
+                      </Col>
+                      <Col lg={8} sm={24}>
+                        <div className="flex justify-center gap-4">
+                          <button
+                            className="py-3 px-10 rounded-full bg-blue-600 hover:bg-blue-800 hover:duration-300 hover:shadow-xl text-white"
+                            onClick={() => {
+                              getBusesByFilter();
+                            }}
+                          >
+                            Search
+                          </button>
+                        </div>
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className="z-20">
+                    <Row gutter={[15, 15]}>
+                      {buses
+                        .filter((bus) => {
+                          if (
+                            !filters.from &&
+                            !filters.to &&
+                            !filters.journeyDate
+                          ) {
+                            return (
+                              bus.from === filters.from &&
+                              bus.to === filters.to &&
+                              bus.journeyDate === filters.journeyDate
+                            );
+                          } else {
+                            return bus;
+                          }
+                        })
+                        .map((bus, index) => {
+                          return (
+                            <Col key={index} lg={24} sm={24}>
+                              <Bus bus={bus} />
+                            </Col>
+                          );
+                        })}
+                      {buses.length === 0 && (
+                        <div className="flex justify-center w-full">
+                          <h1 className="text-2xl font-bold text-gray-500">
+                            No buses found
+                          </h1>
+                        </div>
+                      )}
+                    </Row>
+                  </div>
                 </div>
               </div>
             </div>
