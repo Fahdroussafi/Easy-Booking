8d4d29853de3d8ca3f8fc81dd47af933f60f3750
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-04 15:37:44 +0100

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, message } from "antd";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -19,7 +19,17 @@ function Login() {
         localStorage.setItem("token", response.data.data);
         localStorage.setItem("user_id", response.data.user._id);
 
-        navigate("/easy-booking");
+        const idTrip = localStorage.getItem("idTrip");
+        const idUser = localStorage.getItem("user_id");
+
+        if (response.data.user.isAdmin === true) {
+          navigate("/admin/buses");
+        } else if (idTrip == null) {
+          navigate("/bookings");
+        } else if (idTrip !== null) {
+          navigate(`/book-now/${idTrip}`);
+        }
+
       } else {
         message.error(response.data.message);
       }
@@ -33,9 +43,9 @@ function Login() {
       <Helmet>
         <title>Login</title>
       </Helmet>
-      <Form onFinish={onFinish}>
+      <Form onFinish={onFinish} className="h-screen flex">
         <div
-          className="bg-white min-h-screen flex flex-col "
+          className="hidden lg:flex w-full lg:w-3/4"
           style={{
             backgroundSize: "cover",
             backgroundImage: `url("https://img5.goodfon.com/wallpaper/nbig/6/6a/temsa-maraton-coach-bus.jpg")`,
@@ -43,64 +53,64 @@ function Login() {
             backgroundRepeat: "no-repeat",
           }}
         >
-          <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-            <div className="bg-gray-50 bg-opacity-80 px-6 py-8 rounded-xl shadow-xl text-black w-full border-2">
-              <h1 className="mb-8 text-3xl text-center font-bold">LOGIN</h1>
-              <Form.Item
-                name="email"
-                initialValue=""
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your email!",
-                    validateTrigger: "onSubmit",
-                  },
-                ]}
+          <div className="flex items-center h-full w-full px-20 bg-gray-900 bg-opacity-40"></div>
+        </div>
+
+        <div className="from-gray-50 to-blue-500 bg-gradient-to-r flex w-full lg:w-1/2 justify-center items-center space-y-8">
+          <div className="w-3/4 px-8 md:px-32 lg:px-24">
+            <h1 className="mb-8 text-5xl text-center font-bold italic">
+              Login
+            </h1>
+            <Form.Item
+              name="email"
+              initialValue=""
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                  validateTrigger: "onSubmit",
+                },
+              ]}
+            >
+              <input
+                type="email"
+                className="block border-2 border-blue-700 w-full p-3 px-5 rounded-full mb-4"
+                placeholder="Email"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              initialValue=""
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                  min: 6,
+                  validateTrigger: "onSubmit",
+                },
+              ]}
+            >
+              <input
+                type="password"
+                className="block border-2 border-blue-700 w-full p-3 px-5 rounded-full mb-4"
+                placeholder="Password"
+                autoComplete="off"
+              />
+            </Form.Item>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
               >
-                <input
-                  type="email"
-                  className="block border border-black w-full p-3 rounded mb-4"
-                  placeholder="Email"
-                />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                initialValue=""
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                    min: 6,
-                    validateTrigger: "onSubmit",
-                  },
-                ]}
-              >
-                <input
-                  type="password"
-                  className="block border border-black w-full p-3 rounded mb-4"
-                  placeholder="Password"
-                  autoComplete="off"
-                />
-              </Form.Item>
-              <div className="flex justify-center">
-                <button
-                  type="submit"
-                  className=" bg-blue-600 rounded-2xl font-bold w-56 text-center py-3 text-white hover:text-black hover:bg-white hover:bg-white-500 hover:border-black hover:border duration-500 focus:outline-none my-1"
-                >
-                  LOGIN
-                </button>
-              </div>
-              <p className="text-center text-base text-gray-600 mt-4">
-                Don't have an account?
-                <a
-                  className="no-underline border-b border-gray-500 text-gray-500"
-                  href="/register"
-                >
-                  {" "}
-                  Register
-                </a>
-              </p>
+                LOGIN
+              </button>
             </div>
+            <p className="text-center text-base text-gray-600">
+              Don't have an account?
+              <a className="btn btn-link text-blue-600" href="/register">
+                Register
+              </a>
+            </p>
           </div>
         </div>
       </Form>
