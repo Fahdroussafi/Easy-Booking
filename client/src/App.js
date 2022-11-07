e2766b8afd74c50f32e9c40869e94a82580e7a98
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 00:40:57 +0100

    lazy load

--- client/src/App.js
+++ client/src/App.js
@@ -1,117 +1,120 @@
+import React, { lazy, Suspense } from "react";
 import "./index.css";
 import "antd/dist/antd.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import BookNow from "./pages/BookNow";
-import Bookings from "./pages/Bookings";
-import Profile from "./pages/Profile";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Loader from "./components/Loader";
 import PublicRoute from "./components/PublicRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
-import AdminBuses from "./pages/Admin/AdminBuses";
-import AdminUsers from "./pages/Admin/AdminUsers";
-import AdminBookings from "./pages/Admin/AdminBookings";
+
+const Index = lazy(() => import("./pages/Index"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const AdminBookings = lazy(() => import("./pages/Admin/AdminBookings"));
+const AdminBuses = lazy(() => import("./pages/Admin/AdminBuses"));
+const AdminUsers = lazy(() => import("./pages/Admin/AdminUsers"));
+const Home = lazy(() => import("./pages/Home"));
+const BookNow = lazy(() => import("./pages/BookNow"));
+const Bookings = lazy(() => import("./pages/Bookings"));
+const Profile = lazy(() => import("./pages/Profile"));
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
-    <div className="App bg-slate-100">
+    <div className="App">
       {loading && <Loader />}
       <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PublicRoute>
-                <Index />
-              </PublicRoute>
-            }
-          />
+        <Suspense fallback={loading}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <Index />
+                </PublicRoute>
+              }
+            />
 
-          <Route
-            path="/easy-booking"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/easy-booking"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/bookings"
-            element={
-              <ProtectedRoute>
-                <Bookings />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/bookings"
+              element={
+                <ProtectedRoute>
+                  <Bookings />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/book-now/:id"
-            element={
-              <ProtectedRoute>
-                <BookNow />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/book-now/:id"
+              element={
+                <ProtectedRoute>
+                  <BookNow />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/admin/bookings"
-            element={
-              <ProtectedRoute>
-                <AdminBookings />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/admin/bookings"
+              element={
+                <ProtectedRoute>
+                  <AdminBookings />
+                </ProtectedRoute>
+              }
+            />
 
+            <Route
+              path="/admin/buses"
+              element={
+                <ProtectedRoute>
+                  <AdminBuses />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <AdminUsers />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/admin/buses"
-            element={
-              <ProtectedRoute>
-                <AdminBuses />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/users"
-            element={
-              <ProtectedRoute>
-                <AdminUsers />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/login"
-            element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <PublicRoute>
-                <Register />
-              </PublicRoute>
-            }
-          />
-        </Routes>
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
