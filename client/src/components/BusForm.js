50b0a68af6e138a1f733b9f3cc20fcccee06972c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 19:41:52 +0100

    Button changes

--- client/src/components/BusForm.js
+++ client/src/components/BusForm.js
@@ -263,9 +263,16 @@ function BusForm({
         <div className="flex justify-end">
           <button
             type="submit"
-            className="btn btn-primary bg-blue-600 hover:bg-blue-800"
+            className="relative inline-flex items-center justify-start
+                px-10 py-3 overflow-hidden font-bold rounded-full
+                group"
           >
-            Save
+            <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+            <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
+            <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
+              Save
+            </span>
+            <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
           </button>
         </div>
       </Form>
