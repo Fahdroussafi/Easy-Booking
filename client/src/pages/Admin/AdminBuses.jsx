9c96b592ecf6ebe79eb82c4ea74ee202bb763fbd
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 19:40:20 +0100

    UI changes

--- client/src/pages/Admin/AdminBuses.jsx
+++ client/src/pages/Admin/AdminBuses.jsx
@@ -72,6 +72,15 @@ function AdminBuses() {
     {
       title: "Status",
       dataIndex: "status",
+      render: (status) => {
+        if (status === "Completed") {
+          return <span className="text-red-500">{status}</span>;
+        } else if (status === "running") {
+          return <span className="text-yellow-500">{status}</span>;
+        } else {
+          return <span className="text-green-500">{status}</span>;
+        }
+      },
     },
     {
       title: "Action",
@@ -79,12 +88,12 @@ function AdminBuses() {
       render: (actions, record) => (
         <div className="flex gap-3">
           <i
-            className="ri-delete-bin-line cursor-pointer"
+            className="ri-delete-bin-line cursor-pointer text-red-500 text-xl"
             onClick={() => deleteBus(record._id)}
           ></i>
 
           <i
-            className="ri-pencil-line cursor-pointer"
+            className="ri-pencil-line cursor-pointer text-xl"
             onClick={() => {
               setSelectedBus(record);
               setShowBusForm(true);
@@ -108,10 +117,18 @@ function AdminBuses() {
         <div className="flex justify-between p-7">
           <PageTitle title="Buses" />
           <button
-            className="btn btn-primary bg-blue-600 hover:bg-blue-800"
+            type="submit"
+            className="relative inline-flex items-center justify-start
+                px-10 py-3 overflow-hidden font-bold rounded-full
+                group"
             onClick={() => setShowBusForm(true)}
           >
-            Add Bus
+            <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+            <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
+            <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
+              Add Bus
+            </span>
+            <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
           </button>
         </div>
         <div className="p-7">
