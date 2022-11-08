67e759ae6569d80a897a170e2232efabd978cb01
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 19:42:08 +0100

    button changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -118,12 +118,19 @@ function Home() {
             <Col lg={8} sm={24}>
               <div className="flex justify-center gap-4">
                 <button
-                  className="py-3 px-10 rounded-full bg-blue-600 hover:bg-blue-800 hover:duration-300 hover:shadow-xl text-white"
                   onClick={() => {
                     getBusesByFilter();
                   }}
+                  className="relative inline-flex items-center justify-start
+                    px-10 py-3 overflow-hidden font-bold rounded-full
+                    group"
                 >
-                  Search
+                  <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                  <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
+                  <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
+                    Search
+                  </span>
+                  <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
                 </button>
               </div>
             </Col>
@@ -160,6 +167,7 @@ function Home() {
           </Row>
         </div>
       </div>
+  
     </>
   );
 }
