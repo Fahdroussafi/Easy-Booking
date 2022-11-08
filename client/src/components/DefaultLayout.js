3935ac47a262765c0791e01fc620ee1f0e37f4d2
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-09 00:49:33 +0100

    code cleanup

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -126,19 +126,6 @@ function DefaultLayout({ children }) {
           })}
         </div>
       </div>
-
-      {/* <button
-        type="submit"
-        className="relative inline-flex items-center justify-start px-10 py-3 overflow-hidden font-bold rounded-full group"
-      >
-        <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-        <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
-        <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
-          Login
-        </span>
-        <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
-      </button> */}
-
       <div className="w-full">
         <div className="bg-gray-800 flex flex-col justify-start items-center py-2">
           <img
