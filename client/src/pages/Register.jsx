b3f8f62d24183a3a1accf5b55eddba5b820ea048
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 19:40:01 +0100

    Button UI changed

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -45,6 +45,33 @@ function Register() {
           </div>
           <div className="from-gray-50 to-blue-500 bg-gradient-to-r flex w-full lg:w-1/2 justify-center items-center space-y-8">
             <div className="w-3/4 px-8 md:px-32 lg:px-24">
+              <div className="flex flex-col items-center mb-10">
+                <Link
+                  to="/"
+                  className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-blue-600 transition duration-300 ease-out border-2 border-blue-600 rounded-full shadow-md group"
+                >
+                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-600 group-hover:translate-x-0 ease">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                      />
+                    </svg>
+                  </span>
+                  <span className="absolute flex items-center justify-center w-full h-full text-blue-600 transition-all duration-300 transform group-hover:translate-x-full ease">
+                    Back
+                  </span>
+                  <span className="relative invisible">Button Text</span>
+                </Link>
+              </div>
               <h1 className="mb-8 text-5xl text-center font-bold italic">
                 Register
               </h1>
@@ -106,12 +133,19 @@ function Register() {
                   autoComplete="off"
                 />
               </Form.Item>
-              <div className="flex justify-center">
+              <div className="flex justify-center mb-5">
                 <button
                   type="submit"
-                  className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
+                  className="relative inline-flex items-center justify-start
+                  px-8 py-3 overflow-hidden font-bold rounded-full
+                  group"
                 >
-                  Create Account
+                  <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                  <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
+                  <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
+                    Create Account
+                  </span>
+                  <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
                 </button>
               </div>
               <p className="text-center text-base text-gray-600 mt-4">
