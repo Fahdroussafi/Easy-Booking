afe74334ea2f66e05ce1c9b16b24aad61b8ed1b6
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 19:41:27 +0100

    UI and logix changes

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -78,38 +78,19 @@ function Index() {
           className="hero min-h-screen lg:flex w-full lg:w-3/4"
           style={{
             backgroundImage: `url("https://cdn.dribbble.com/users/1976094/screenshots/4687414/buss_trvl.gif")`,
-            backgroundPosition: "center center",
-            backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
+            backgroundPosition: "center center",
           }}
         >
           <div className="flex items-center h-full w-full">
             <div className="h-screen overflow-auto overflow-x-hidden">
               <div className="bg-opacity-80">
                 <Row gutter={[15, 15]}>
-                  {buses
-                    .filter((bus) => {
-                      if (
-                        !filters.from &&
-                        !filters.to &&
-                        !filters.journeyDate
-                      ) {
-                        return (
-                          bus.from === filters.from &&
-                          bus.to === filters.to &&
-                          bus.journeyDate === filters.journeyDate
-                        );
-                      } else {
-                        return bus;
-                      }
-                    })
-                    .map((bus, index) => {
-                      return (
-                        <Col key={index} lg={24} sm={24}>
-                          <Bus bus={bus} />
-                        </Col>
-                      );
-                    })}
+                  {buses.map((bus) => (
+                    <div className="w-screen  ">
+                      <Bus bus={bus} />
+                    </div>
+                  ))}
                 </Row>
               </div>
             </div>
@@ -135,17 +116,22 @@ function Index() {
             </p>
             <Link
               to="/login"
-              className="block w-full bg-blue-600 mt-4 py-2 rounded-2xl
-              hover:bg-blue-700 hover:-translate-y-1 transition-all duration-500
-              text-white font-semibold mb-2"
+              className="relative inline-flex items-center justify-start
+                px-10 py-3 overflow-hidden font-bold rounded-full
+                group"
             >
-              Check your bookings now
+              <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+              <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:translate-x-1"></span>
+              <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
+                Check your tickets
+              </span>
+              <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
             </Link>
             <div className="w-full my-5 mx-2 p-2 px-2 py-3 flex justify-center">
               <Row gutter={10} align="center">
                 <Col lg={12} sm={24}>
                   <select
-                    className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mb-5 select select-primary w-full max-w-xs"
                     onChange={(e) => {
                       setFilters({ ...filters, from: e.target.value });
                     }}
@@ -162,7 +148,7 @@ function Index() {
                 </Col>
                 <Col lg={12} sm={24}>
                   <select
-                    className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mb-5 select select-primary w-full max-w-xs"
                     onChange={(e) => {
                       setFilters({ ...filters, to: e.target.value });
                     }}
@@ -179,7 +165,7 @@ function Index() {
                 </Col>
                 <Col lg={24} sm={24}>
                   <input
-                    className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mb-5 input input-bordered input-primary w-full max-w-xs"
                     min={new Date().toISOString().split("T")[0]}
                     type="date"
                     placeholder="Date"
@@ -191,22 +177,30 @@ function Index() {
                     }}
                   />
                 </Col>
-                <Col lg={8} sm={24}>
+                <Col lg={24} sm={24}>
                   <div className="flex justify-center gap-4">
                     <button
-                      className="py-3 px-10 rounded-full bg-blue-600 hover:bg-blue-800 hover:duration-300 hover:shadow-xl text-white"
                       onClick={() => {
                         getBusesByFilter();
+                        // getBuses();
                       }}
+                      className="relative inline-flex items-center justify-start
+                    px-10 py-3 overflow-hidden font-bold rounded-full
+                    group"
                     >
-                      Search
+                      <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                      <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
+                      <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
+                        Search
+                      </span>
+                      <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
                     </button>
                   </div>
                 </Col>
                 <div className="flex justify-center gap-4 mt-5 w-full">
                   {buses.length === 0 && (
                     <div className="text-center text-white text-2xl">
-                      No buses found
+                      Search for a bus
                     </div>
                   )}
                 </div>
