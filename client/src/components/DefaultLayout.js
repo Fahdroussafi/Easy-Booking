fbf579864cafb9b11f9da6e5027444a5f096e2a3
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-08 19:40:55 +0100

    UI changes

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -92,10 +92,15 @@ function DefaultLayout({ children }) {
               <div
                 key={key}
                 className={`${
-                  activeRoute === item.path && "rounded-xl bg-blue-800"
-                } text-base gap-5 mr-[10px] text-white flex items-center hover:bg-blue-800 hover:rounded-xl duration-200 justify-start py-[5px] px-[15px] w-full cursor-pointer transition-[0.2s]`}
+                  activeRoute === item.path && "bg-blue-900 btn-disabled"
+                } gap-2 relative inline-flex items-center justify-start px-10 py-3 overflow-hidden font-bold rounded-full group`}
               >
-                <i className={item.icon}></i>
+                <i
+                  className={`${item.icon} w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-black opacity-[3%]`}
+                ></i>
+                <i
+                  className={`${item.icon} text-white text-[20px] group-hover:text-black`}
+                ></i>
 
                 {!collapsed && (
                   <span
@@ -108,7 +113,12 @@ function DefaultLayout({ children }) {
                       }
                     }}
                   >
-                    {item.name}
+                    <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                    <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:translate-x-0"></span>
+                    <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-black">
+                      {item.name}
+                    </span>
+                    <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
                   </span>
                 )}
               </div>
@@ -116,6 +126,18 @@ function DefaultLayout({ children }) {
           })}
         </div>
       </div>
+
+      {/* <button
+        type="submit"
+        className="relative inline-flex items-center justify-start px-10 py-3 overflow-hidden font-bold rounded-full group"
+      >
+        <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+        <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
+        <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
+          Login
+        </span>
+        <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
+      </button> */}
 
       <div className="w-full">
         <div className="bg-gray-800 flex flex-col justify-start items-center py-2">
