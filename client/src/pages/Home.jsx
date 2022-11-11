7b0c09e0e6fe2b629912c1af47ef50756bec3678
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-11 15:37:42 +0100

    minor UI change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -51,7 +51,7 @@ function Home() {
           <Row gutter={10} align="center">
             <Col lg={12} sm={24}>
               <select
-                className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="mb-5 select select-primary bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white"
                 onChange={(e) => {
                   setFilters({ ...filters, from: e.target.value });
                 }}
@@ -68,7 +68,7 @@ function Home() {
             </Col>
             <Col lg={12} sm={24}>
               <select
-                className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="mb-5 select select-primary bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white "
                 onChange={(e) => {
                   setFilters({ ...filters, to: e.target.value });
                 }}
@@ -85,7 +85,7 @@ function Home() {
             </Col>
             <Col lg={24} sm={24}>
               <input
-                className="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="mb-5 input input-primary bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white "
                 min={new Date().toISOString().split("T")[0]}
                 type="date"
                 placeholder="Date"
