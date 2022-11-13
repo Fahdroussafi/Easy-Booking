131aeb2f0f4778672892a38bea95ed284cc1e3ee
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-14 00:34:44 +0100

    UI minor changes

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -19,6 +19,7 @@ function ForgotPassword() {
       if (response.status === 200) {
         dispatch(HideLoading());
         message.success("Email Sent Successfully Please Check Your Email");
+        localStorage.setItem("email", values.email);
         navigate("/email-sent");
       }
     } catch (error) {
@@ -32,14 +33,14 @@ function ForgotPassword() {
         <title>Forgot Password</title>
       </Helmet>
 
-      <Form onFinish={onFinish} classNameName="h-screen flex ">
+      <Form onFinish={onFinish}>
         <div className="from-gray-50 to-blue-500 bg-gradient-to-r h-screen flex items-center justify-center p-12 py-6">
           <div className="mx-auto w-full max-w-screen-lg bg-blue-700 px-5 py-10 rounded-full">
             <div className="grid gap-5 md:grid-cols-2 md:gap-10 lg:gap-20">
               <div className="flex justify-center md:justify-end">
                 <img
-                  className="w-full max-w-sm"
-                  src="https://ouch-cdn2.icons8.com/sKnF2PmYhkmP28DzIm6KqWSknT03UVWjg3FLlGYIOp4/rs:fit:684:456/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTI3/L2U4OWQ2NmZiLTg0/NzEtNDc3NS1hNTA0/LTMwNWRiYmJkNzg0/MC5zdmc.png"
+                  className="w-full max-w-sm rounded-full"
+                  src="https://img.freepik.com/free-vector/forgot-password-concept-illustration_114360-4652.jpg?w=2000"
                   alt="Marketing newsletter via computer Illustration in PNG, SVG"
                 />
               </div>
