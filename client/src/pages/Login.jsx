f56cafa1083d834713bc56cb6c12a0ee5d7733a6
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-13 17:54:57 +0100

    show password toggle function

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, message } from "antd";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
@@ -35,6 +35,14 @@ function Login() {
       dispatch(HideLoading());
       message.error(error.message);
     }
+  };
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  // Password toggle handler
+  const TogglePassword = () => {
+    // When the handler is invoked
+    // inverse the boolean state of passwordShown
+    setPasswordShown(!passwordShown);
   };
   return (
     <>
@@ -126,10 +134,9 @@ function Login() {
             >
               <div className="relative z-0 mb-6 w-full group">
                 <input
-                  type="password"
+                  type={passwordShown ? "text" : "password"}
                   name="floating_password"
-                  id="floating_password"
-                  className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-blue-500 dark:focus:border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black dark:border-blue-500 dark:focus:border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
@@ -139,6 +146,47 @@ function Login() {
                 >
                   Password
                 </label>
+                <i
+                  className="absolute right-0 top-0 mt-3 mr-4 text-black cursor-pointer"
+                  onClick={TogglePassword}
+                >
+                  {passwordShown ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  )}
+                </i>
               </div>
             </Form.Item>
             <div className="flex justify-center mb-5">
@@ -166,12 +214,11 @@ function Login() {
               </Link>
             </p>
             <p className="text-center text-base text-gray-600">
-              Forgot Password ?{" "}
               <Link
                 to="/forgot-password"
                 className="text-blue-600 font-bold hover:text-blue-700"
               >
-                Reset it
+                Forgot Password ?
               </Link>
             </p>
           </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Form, message } from "antd";
 import axios from "axios";
@@ -10,6 +10,12 @@ function Register() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const onFinish = async (values) => {
+    // compare password and confirm password
+    if (values.password !== values.confirmPassword) {
+      message.error("Password and Confirm Password must be same");
+      return;
+    }
+
     try {
       dispatch(ShowLoading());
       const response = await axios.post("/api/auth/create-user", values);
@@ -25,6 +31,15 @@ function Register() {
       message.error(error.message);
     }
   };
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  // Password toggle handler
+  const TogglePassword = () => {
+    // When the handler is invoked
+    // inverse the boolean state of passwordShown
+    setPasswordShown(!passwordShown);
+  };
+
   return (
     <>
       <Helmet>
@@ -132,27 +147,23 @@ function Register() {
                   </label>
                 </div>
               </Form.Item>
-
               <Form.Item
                 name="password"
                 initialValue=""
                 rules={[
                   {
                     required: true,
-                    message:
-                      "Password must be at least 6 characters! and must contain at least one uppercase letter, one lowercase letter and one number",
+                    message: "Please input your password!",
                     min: 6,
-                    // pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/,
                     validateTrigger: "onSubmit",
                   },
                 ]}
               >
                 <div className="relative z-0 mb-6 w-full group">
                   <input
-                    type="password"
+                    type={passwordShown ? "text" : "password"}
                     name="floating_password"
-                    id="floating_password"
-                    className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-blue-500 dark:focus:border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black dark:border-blue-500 dark:focus:border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required
                   />
@@ -162,6 +173,117 @@ function Register() {
                   >
                     Password
                   </label>
+                  <i
+                    className="absolute right-0 top-0 mt-3 mr-4 text-black cursor-pointer"
+                    onClick={TogglePassword}
+                  >
+                    {passwordShown ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                        />
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                    )}
+                  </i>
+                </div>
+              </Form.Item>
+
+              <Form.Item
+                name="confirmPassword"
+                initialValue=""
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password again!",
+                    min: 6,
+                    validateTrigger: "onSubmit",
+                  },
+                ]}
+              >
+                <div className="relative z-0 mb-6 w-full group">
+                  <input
+                    type={passwordShown ? "text" : "password"}
+                    name="floating_password"
+                    className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black dark:border-blue-500 dark:focus:border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    htmlFor="floating_password"
+                    className="absolute text-sm text-gray-500 dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Confirm Password
+                  </label>
+                  <i
+                    className="absolute right-0 top-0 mt-3 mr-4 text-black cursor-pointer"
+                    onClick={TogglePassword}
+                  >
+                    {passwordShown ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                        />
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                    )}
+                  </i>
                 </div>
               </Form.Item>
               <div className="flex justify-center mb-5">
