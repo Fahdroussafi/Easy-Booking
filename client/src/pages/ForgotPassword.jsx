a683a15f01fd67b178bbd15c1d2730cfa4df4b51
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-13 19:08:39 +0100

    Update ForgotPassword.jsx

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Form, message } from "antd";
 import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ShowLoading, HideLoading } from "../redux/alertsSlice";
 import { Helmet } from "react-helmet";
@@ -31,85 +31,6 @@ function ForgotPassword() {
       <Helmet>
         <title>Forgot Password</title>
       </Helmet>
-      {/* <Form onFinish={onFinish} classNameName="h-screen flex">
-        <div classNameName="from-gray-50 to-blue-500 bg-gradient-to-r flex w-full lg:w-full justify-center items-center space-y-8">
-          <div classNameName=" px-8 md:px-32 lg:px-24">
-            <div classNameName="flex flex-col items-center mb-10">
-              <Link
-                to="/login"
-                classNameName="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-blue-600 transition duration-300 ease-out border-2 border-blue-600 rounded-full shadow-md group"
-              >
-                <span classNameName="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-600 group-hover:translate-x-0 ease">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    classNameName="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                    />
-                  </svg>
-                </span>
-                <span classNameName="absolute flex items-center justify-center w-full h-full text-blue-600 transition-all duration-300 transform group-hover:translate-x-full ease">
-                  Back
-                </span>
-                <span classNameName="relative invisible">Button Text</span>
-              </Link>
-            </div>
-            <h1 classNameName="mb-8 text-5xl text-center font-bold italic">
-              Reset Password
-            </h1>
-            <Form.Item
-              name="email"
-              initialValue=""
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your email!",
-                  validateTrigger: "onSubmit",
-                },
-              ]}
-            >
-              <div classNameName="relative z-0 mb-6 w-full group">
-                <input
-                  type="email"
-                  name="floating_email"
-                  classNameName="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black dark:border-blue-500 dark:focus:border-blue-700 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  htmlFor="floating_email"
-                  classNameName="absolute text-sm text-gray-500 dark:text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Email address
-                </label>
-              </div>
-            </Form.Item>
-
-            <div classNameName="flex justify-center mb-5">
-              <button
-                type="submit"
-                classNameName="relative inline-flex items-center justify-start
-                px-10 py-3 overflow-hidden font-bold rounded-full
-                group"
-              >
-                <span classNameName="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-                <span classNameName="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:translate-x-0"></span>
-                <span classNameName="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
-                  Send Reset Code
-                </span>
-                <span classNameName="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </Form> */}
 
       <Form onFinish={onFinish} classNameName="h-screen flex ">
         <div className="from-gray-50 to-blue-500 bg-gradient-to-r h-screen flex items-center justify-center p-12 py-6">
