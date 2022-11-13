9df169aa1f853ec00622aa7e2ade2a990992cc2c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-14 00:17:30 +0100

    button UI change

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -70,12 +70,16 @@ function ForgotPassword() {
                         placeholder="Enter your email"
                         className="w-full rounded border border-white/50 bg-transparent px-3 py-2 text-white placeholder:text-white/50 md:max-w-[18rem]"
                       />
-                      <button
-                        type="submit"
-                        className="mt-4 w-full max-w-[14rem] rounded bg-white/30 px-14 py-2 text-center text-white"
-                      >
-                        Send Reset Code
-                      </button>
+                      <div className="mt-5">
+                        <button className="relative inline-flex items-center justify-start px-10 py-3 overflow-hidden font-bold rounded-full group">
+                          <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                          <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-gray-100 opacity-100 group-hover:translate-x-0"></span>
+                          <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-black">
+                            Send Reset Code
+                          </span>
+                          <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
+                        </button>
+                      </div>
                     </div>
                   </Form.Item>
                 </div>
