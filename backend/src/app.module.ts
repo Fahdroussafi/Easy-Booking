b4403452eb0332ed12708bd2e36551e6e0c85abf
myusername <f.roussafi@harmony.ma>
2024-03-27 12:44:13 +0000

    protected routes & made auth routes public

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,17 +3,25 @@ import { UsersModule } from './models/users/users.module';
 import { AuthModule } from './models/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { BusesModule } from './models/buses/buses.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './models/auth/guard';
 
 @Module({
   imports: [
-    UsersModule,
-    AuthModule,
-    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    BusesModule,
   ],
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- backend/src/models/auth/auth.controller.ts
+++ backend/src/models/auth/auth.controller.ts
@@ -1,15 +1,22 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { SignupDto, SigninDto } from './dto/auth.dto';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { Public } from 'src/common/decorators/Public';
 
+@ApiBearerAuth('Autorization')
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
   @Post('signup')
   signUp(@Body() dto: SignupDto) {
     return this.authService.signUp(dto);
   }
 
+  @Public()
   @Post('signin')
   signIn(@Body() dto: SigninDto) {
     return this.authService.signIn(dto);
